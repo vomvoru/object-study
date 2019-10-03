38603fd7e3cf760f825f7192d5b6582d0ea8f015
Park Cheol Hyeon <qkrcjfgus3@gmail.com>
2019-10-03 18:30:04 +0900

    calculateDiscountFee 메소드를 ReservationAgency -> screening

--- chapter4/ReservationAgency.ts
+++ chapter4/ReservationAgency.ts
@@ -1,59 +1,17 @@
 import { Reservation } from './Reservation';
 import { Screening } from './Screening';
 import { Customer } from './Customer';
-import { Movie } from './Movie';
 import { Money } from './Money';
-import { MovieType } from './MovieType';
 
 export class ReservationAgency {
   reserve(screening: Screening, customer: Customer, audienceCount: number): Reservation {
     const discountable: boolean = this.checkDiscountable(screening);
-    const fee: Money = this.calculateDiscountFee(screening, discountable, audienceCount);
+    const fee: Money = screening.calculateDiscountFee(discountable, audienceCount);
     return this.createReservation(customer, screening, fee, audienceCount);
   }
 
   private checkDiscountable(screening: Screening): boolean {
     return screening.isDiscountable();
-  }
-
-  private calculateDiscountFee(screening: Screening, discountable: boolean, audienceCount: number): Money {
-    if (discountable) {
-      return screening
-        .getMovie()
-        .getFee()
-        .minus(this.calculateDiscountedFee(screening.getMovie()))
-        .times(audienceCount);
-    }
-
-    return screening
-      .getMovie()
-      .getFee()
-      .times(audienceCount);
-  }
-
-  private calculateDiscountedFee(movie: Movie): Money {
-    switch (movie.getMovieType()) {
-      case MovieType.ACOUNT_DISCOUNT:
-        return this.calculateAmountDiscountedFee(movie);
-      case MovieType.PERCENT_DISCOUNT:
-        return this.calculatePercentDiscountedFee(movie);
-      case MovieType.NONE_DISCOUNT:
-        return this.calculateNoneDiscountedFee();
-      default:
-        return this.calculateNoneDiscountedFee();
-    }
-  }
-
-  private calculateAmountDiscountedFee(movie: Movie): Money {
-    return movie.getDiscountAmount();
-  }
-
-  private calculatePercentDiscountedFee(movie: Movie): Money {
-    return movie.getFee().times(movie.getDiscountPercent());
-  }
-
-  private calculateNoneDiscountedFee(): Money {
-    return Money.ZERO;
   }
 
   private createReservation(customer: Customer, screening: Screening, fee: Money, audienceCount: number) {
--- chapter4/Screening.ts
+++ chapter4/Screening.ts
@@ -1,9 +1,47 @@
 import { Movie } from './Movie';
+import { Money } from './Money';
+import { MovieType } from './MovieType';
 
 export class Screening {
   private movie: Movie;
   private sequence: number;
   private whenScreended: Date;
+
+  calculateDiscountFee(discountable: boolean, audienceCount: number): Money {
+    if (discountable) {
+      return this.movie
+        .getFee()
+        .minus(this.calculateDiscountedFee(this.movie))
+        .times(audienceCount);
+    }
+
+    return this.movie.getFee().times(audienceCount);
+  }
+
+  private calculateDiscountedFee(movie: Movie): Money {
+    switch (movie.getMovieType()) {
+      case MovieType.ACOUNT_DISCOUNT:
+        return this.calculateAmountDiscountedFee(movie);
+      case MovieType.PERCENT_DISCOUNT:
+        return this.calculatePercentDiscountedFee(movie);
+      case MovieType.NONE_DISCOUNT:
+        return this.calculateNoneDiscountedFee();
+      default:
+        return this.calculateNoneDiscountedFee();
+    }
+  }
+
+  private calculateAmountDiscountedFee(movie: Movie): Money {
+    return movie.getDiscountAmount();
+  }
+
+  private calculatePercentDiscountedFee(movie: Movie): Money {
+    return movie.getFee().times(movie.getDiscountPercent());
+  }
+
+  private calculateNoneDiscountedFee(): Money {
+    return Money.ZERO;
+  }
 
   isDiscountable(): boolean {
     return this.movie.getDiscountConditions().some(condition => condition.isDiscountable(this));
